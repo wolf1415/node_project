30ac2f13f78327df88858af8fce4912bb32c2d7d
Sergey Ghazaryan <sergey.ghazaryan1@outlook.com>
2017-01-21 15:50:00 +0400

    Add customer model

--- api/models/Customer.js
+++ api/models/Customer.js
@@ -8,7 +8,18 @@
 module.exports = {
 
   attributes: {
+    name: {
+      type: 'string',
+      required: true
+    },
+    email: {
+      type: 'string',
+      email: true
+    },
 
+    state: {
+      type: 'string'
+    }
   }
 };
 
