5d73f86690062b0d0981ca39d7cc75bcaccd2f0c
Sergey Ghazaryan <sergey.ghazaryan1@outlook.com>
2017-01-22 12:53:03 +0400

    Add customer model

--- api/controllers/CustomerController.js
+++ api/controllers/CustomerController.js
@@ -6,6 +6,27 @@
  */
 
 module.exports = {
-	
+
+ 'registration': function(req, res) {
+   res.view();
+ },
+
+  create: function(req, res, next) {
+    Customer.create(req.params.all(), function customerCreated(err, customer) {
+      if(err) return next(err);
+      res.redirect('/customer/show/' + custoer.id);
+    });
+  },
+
+  show: function(req, res, next) {
+    Customer.findOne(req.param('id'), function foundCustomer(err, customer) {
+      if(err) return next(err);
+      if(!customer) return next();
+      res.view({
+        customer: customer
+      });
+    });
+  }
+
 };
 
